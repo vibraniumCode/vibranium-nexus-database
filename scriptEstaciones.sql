d28674de21cfd9f1a9eed7c81730b8a47c9f7ad1
misaprostol <camilacapaluna@gmail.com>
2025-10-11 19:50:43 -0300

    actualizacion tabla estaciones

---
+++ scriptEstaciones.sql
@@ -0,0 +1,20 @@
+ALTER TABLE estaciones
+ALTER COLUMN cuit INT;
+
+ALTER TABLE estaciones
+ALTER COLUMN ingBrutos INT;
+
+ALTER TABLE estaciones
+ALTER COLUMN cp INT;
+
+ALTER TABLE estaciones
+ALTER COLUMN telefo INT;
+
+
+
+
+
+
+
+
+
