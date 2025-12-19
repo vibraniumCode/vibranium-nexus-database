5eb45508a8bd99cccefdd2f79c153db55aa7c3bf
Vibranium Code <vibraniumcode@gmail.com>
2025-12-19 16:54:19 -0300

    Refactor empresas table and update defaults

--- tabla/empresas.sql
+++ tabla/empresas.sql
@@ -1,11 +1,14 @@
-USE [NEXUS]
+USE [NexusDB]
 GO
 
-/****** Object:  Table [dbo].[empresas]    Script Date: 26/10/2025 11:55:43 ******/
+/****** Object:  Table [dbo].[empresas]    Script Date: 19/12/2025 16:53:32 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
 GO
 
 CREATE TABLE [dbo].[empresas](
@@ -18,30 +21,28 @@ CREATE TABLE [dbo].[empresas](
 	[localidad] [varchar](50) NULL,
 	[provincia] [varchar](50) NULL,
 	[telefono] [varchar](15) NULL,
-	[Actividad] [date] NULL,
+	[actividad] [date] NULL,
 	[created_by] [int] NULL,
-	[created_at] [datetime2](7) NULL,
+	[created_at] [datetime2](7) NULL DEFAULT (getdate()),
 	[updated_by] [int] NULL,
-	[updated_at] [datetime2](7) NULL,
+	[updated_at] [datetime2](7) NULL DEFAULT (getdate()),
 PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
 UNIQUE NONCLUSTERED 
 (
 	[cuit] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
 UNIQUE NONCLUSTERED 
 (
 	[ingBrutos] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
 GO
 
-ALTER TABLE [dbo].[empresas] ADD  DEFAULT (getdate()) FOR [created_at]
-GO
-
-ALTER TABLE [dbo].[empresas] ADD  DEFAULT (getdate()) FOR [updated_at]
+SET ANSI_PADDING OFF
 GO
 
 
