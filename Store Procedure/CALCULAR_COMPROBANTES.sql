8a3cb13457b9d0e6698bf2653446d80eed7ef292
Vibranium Code <vibraniumcode@gmail.com>
2026-02-16 22:49:55 -0300

    Update CALCULAR_COMPROBANTES.sql

--- Store Procedure/CALCULAR_COMPROBANTES.sql
+++ Store Procedure/CALCULAR_COMPROBANTES.sql
@@ -1,35 +1,28 @@
 USE [NexusDB]
 GO
-/****** Object:  StoredProcedure [dbo].[CALCULAR_COMPROBANTES]    Script Date: 25/01/2026 18:05:17 ******/
+/****** Object:  StoredProcedure [dbo].[CALCULAR_COMPROBANTES]    Script Date: 16/02/2026 18:53:43 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-/*
-BEGIN TRAN
-	EXEC CALCULAR_COMPROBANTES
-		@IdEmpresa = 50,
-		@ImporteTotal = 50000,
-		@LitrosPromedio = 50,
-		@MargenLitros = 2,
-		@ImporteMin = 4000,
-		@ImporteMax = 12000,
-		@IdCombustible=3
-ROLLBACK
-*/
- ALTER PROCEDURE [dbo].[CALCULAR_COMPROBANTES]
+
+--exec [CALCULAR_COMPROBANTES] 50,1,100000,50,2,10000,20000,8,'20260101','20260228'
+
+ALTER PROCEDURE [dbo].[CALCULAR_COMPROBANTES]
 	@IdEmpresa INT,
+	@IdCliente INT,
     @ImporteTotal DECIMAL(18,2),
     @LitrosPromedio DECIMAL(10,2),
     @MargenLitros DECIMAL(10,2) = 2,
     @ImporteMin DECIMAL(18,2),
     @ImporteMax DECIMAL(18,2),
-	@IdCombustible INT
-    --@PrecioPorLitro DECIMAL(18,4) = 10000  -- precio por litro fijo opcional
+	@IdCombustible INT,
+	@FechaDesde DATE,
+	@FechaHasta DATE,
+	@MargenDias INT = 2
 AS
 BEGIN
     SET NOCOUNT ON;
-
     DECLARE 
         @Restante DECIMAL(18,2) = @ImporteTotal,
         @Litros DECIMAL(10,2),
@@ -39,75 +32,113 @@ BEGIN
         @LitroMax DECIMAL(10,2),
 		@nroTiquet INT,
 		@grupoFactura INT,
-		@FechaActual VARCHAR(10) = CONVERT(VARCHAR(10), CAST(SYSDATETIMEOFFSET() AT TIME ZONE 'Argentina Standard Time' AS DATE), 103),
+		@FechaActual VARCHAR(10),
 		@HoraActual VARCHAR(8) = CONVERT(VARCHAR(8), DATEADD(HOUR, +3, GETDATE()), 108),
-		@UltFactura INT
-
+		@UltFactura INT,
+		@FechaFactura DATE,
+		@FechaAnterior DATE,
+		@DiasDisponibles INT,
+		@DiaAleatorio INT,
+		@DiaMin INT,
+		@DiaMax INT
+    
+    -- Validar que FechaDesde sea menor que FechaHasta
+    IF @FechaDesde > @FechaHasta
+    BEGIN
+        RAISERROR('La fecha desde no puede ser mayor que la fecha hasta', 16, 1);
+        RETURN;
+    END;
+    
     SET @LitroMin = @LitrosPromedio - @MargenLitros;
     SET @LitroMax = @LitrosPromedio + @MargenLitros;
-	SET @grupoFactura = (SELECT CASE WHEN ID_GRUPO = 1 THEN 1 ELSE ID_GRUPO + 1 END FROM GRUPO_FACTURA)
-	SET @UltFactura = (SELECT MAX(n_factura) from [COMPROBANTE_HISTORICO] WHERE idEstacion = @IdEmpresa) 
-	--select @UltFactura, '@UltFactura'
-
+	SET @grupoFactura = (SELECT CASE WHEN ID_GRUPO = 1 THEN 1 ELSE ID_GRUPO + 1 END FROM GRUPO_FACTURA);
+	SET @UltFactura = (SELECT ISNULL(MAX(n_factura), 0) 
+                   FROM [COMPROBANTE_HISTORICO] 
+                   WHERE idEstacion = @IdEmpresa);
+	SET @FechaAnterior = @FechaDesde;
+	SET @DiasDisponibles = DATEDIFF(DAY, @FechaDesde, @FechaHasta);
+    
     DECLARE @Comprobantes TABLE (
         NroComprobante INT,
         Litros DECIMAL(10,2),
-        Importe DECIMAL(18,2)
+        Importe DECIMAL(18,2),
+        FechaComprobante DATE
     );
-
+    
     WHILE @Restante > 0
     BEGIN
         -- Litros aleatorios entre el rango permitido (48 a 52 si promedio=50 y margen=2)
         SET @Litros = ROUND(RAND() * (@LitroMax - @LitroMin) + @LitroMin, 2);
-
+        
         -- Importe aleatorio entre el rango definido
         SET @Importe = ROUND(RAND() * (@ImporteMax - @ImporteMin) + @ImporteMin, 2);
-
+        
         -- Si supera lo que queda, lo ajustamos
         IF @Importe > @Restante
             SET @Importe = @Restante;
-
+        
+        -- Generar fecha con margen de días (similar a litros y importes)
+        -- Rango: @FechaAnterior + 1 día hasta @FechaAnterior + MargenDias
+        --IF @FechaAnterior >= @FechaHasta
+        --BEGIN
+        --    RAISERROR('No hay más fechas disponibles dentro del rango. Se generaron %d comprobantes.', 16, 1, @ComprobanteNro);
+        --    BREAK;
+        --END;
+        
+        SET @DiaMin = 1;  -- Mínimo 1 día adelante (progresivo)
+        SET @DiaMax = @MargenDias;
+        
+        -- Generar salto aleatorio entre DiaMin y DiaMax
+        SET @DiaAleatorio = CAST(RAND() * (@DiaMax - @DiaMin + 1) + @DiaMin AS INT);
+        SET @FechaFactura = DATEADD(DAY, @DiaAleatorio, @FechaAnterior);
+        
+        -- Si supera el límite, ajustar a la fecha final
+        IF @FechaFactura > @FechaHasta
+            SET @FechaFactura = @FechaHasta;
+        
+        SET @FechaActual = CONVERT(VARCHAR(10), @FechaFactura, 103);
+        
 		EXEC @nroTiquet = sp_crearFactura @IdEmpresa;
-
-        INSERT INTO @Comprobantes (NroComprobante ,Litros, Importe)
-        VALUES (@nroTiquet, @Litros, @Importe);
-
+        
+        INSERT INTO @Comprobantes (NroComprobante, Litros, Importe, FechaComprobante)
+        VALUES (@nroTiquet, @Litros, @Importe, @FechaFactura);
+        
         SET @Restante -= @Importe;
         SET @ComprobanteNro += 1;
-
+        SET @FechaAnterior = @FechaFactura;
+        
         IF @ComprobanteNro > 1000 BREAK;
     END;
-
-    -- Resultado detallado
+    
+    -- Insertar en histórico
 	INSERT INTO COMPROBANTE_HISTORICO
     SELECT 
         NroComprobante,
         Litros,
         Importe,
 		@grupoFactura,
-		@FechaActual,
+		FechaComprobante,
 		@HoraActual,
-		@IdEmpresa
-		--CAST(@FechaActual AS DATE),  
-		--CONVERT(VARCHAR(8), DATEADD(HOUR, +3, GETDATE()), 108)
-
+		@IdEmpresa,
+		@IdCliente
     FROM @Comprobantes;
-	--select * from COMPROBANTE_HISTORICO order by 4 desc
-	UPDATE GRUPO_FACTURA SET ID_GRUPO = ID_GRUPO + 1
 	
+	UPDATE GRUPO_FACTURA SET ID_GRUPO = ID_GRUPO + 1;
+	
+    -- Resultado detallado
 	SELECT 
 		N_FACTURA AS NroComprobante,
 		N_LITROS  AS Litros,
-		IMPORTE   AS Importe 
+		IMPORTE   AS Importe,
+		FECHA AS FechaFactura
 	FROM COMPROBANTE_HISTORICO
 	WHERE 
 		idEstacion = @IdEmpresa AND
-		N_FACTURA > @UltFactura ;
-
+		N_FACTURA > @UltFactura;
+    
     -- Resultado resumen
     SELECT 
         COUNT(*) AS CantidadComprobantes,
         SUM(Importe) AS TotalCalculado
-    FROM @Comprobantes
-
+    FROM @Comprobantes;
 END;
